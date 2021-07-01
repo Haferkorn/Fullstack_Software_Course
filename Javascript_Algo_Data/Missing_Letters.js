489f8dbe103c9dce5e861e7781fc8bdffe84d088
Haferkorn <anette.haferkorn@web.de>
2021-07-01 17:05:13 +0200

    Update Missing_Letters.js

--- Javascript_Algo_Data/Missing_Letters.js
+++ Javascript_Algo_Data/Missing_Letters.js
@@ -1,11 +1,11 @@
 const range=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 function defineFirstIndex(str){
-  let index=range.indexOf(str[0])
+  let index=range.indexOf(str[0])-1
   return index;
 }
 function defineLastIndex(str){
-  let index=range.indexOf(str[str.length-1])
+  let index=range.indexOf(str[str.length-1])-1
   return index;
 }
 
@@ -14,7 +14,8 @@ function fearNotLetter(str) {
   let end=defineLastIndex(str);
  while(start<=end){
    start+=1
-   
+   console.log(start)
+
  }
 }
 
